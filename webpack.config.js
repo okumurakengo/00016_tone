27fe8500f9cddfe902173163cee851b77acc120c
okumurakengo <okumurakengo1a@gmail.com>
2019-04-07 17:15:50 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const glob = require("glob");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: glob.sync("./src/*").reduce((entries, entry) => {
+        return { ...entries, ...{ [entry.replace(/(\/src|\.js|\.ts)/g, "")]: entry }};
+    }, {}),
+    output: {
+        path: `${__dirname}/dist`,
+        filename: "[name].js",
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            loader: ["ts-loader"],
+        },
+    ]},
+    plugins: glob.sync("./src/*").map(entry => {
+        return new HtmlWebpackPlugin({
+            chunks: [entry.replace(/(\/src|\.js|\.ts)/g, "")],
+            filename: `${entry.replace(/(\/src|\.js|\.ts)/g, "")}.html`,
+        })
+    }),
+    devServer: {
+        contentBase: `${__dirname}/dist`,
+        compress: true,
+        port: 9000
+    },
+};
